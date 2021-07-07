93eb24d3a2c1d1ee89ca3948d344b7c1b8d08a48
cizzles <67773700+cizzles@users.noreply.github.com>
2021-07-07 02:46:55 -0500

    Create Information.lua

---
+++ Trojan Technologies Ranking Center/Modules/Information.lua
@@ -0,0 +1,33 @@
+--[[
+TROJAN TECHNOLOGIES
+Trojan.lua
+Vanilla
+1.0.0
+cizzles
+]]--
+
+--> PRESET INFORMATION; DO NOT CONFIGURE
+
+local Information = {
+
+	Portal = 5470658045,
+	
+	Version_Name = "Vanilla",
+	
+	V = "1.0.0",
+
+	Center_Type = "Ranking Centers",
+	
+	Package = 0,
+
+	Add_Ons = {
+		
+		{0, 0};
+		
+	},
+	
+}	
+
+return Information
+
+--> Copyright 2020-2021 Trojan Technologies. © All rights reserved.
