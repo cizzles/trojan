14c09c52455196744f1bbb87a60b7827a04f11a5
cizzles <67773700+cizzles@users.noreply.github.com>
2021-07-09 01:36:44 -0500

    Create Information.lua

---
+++ Trojan Technologies Support-A-Creator Center/Modules/Information.lua
@@ -0,0 +1,32 @@
+--[[
+TROJAN TECHNOLOGIES
+Trojan.lua
+Vanilla
+1.0.0
+]]--
+
+--> PRESET INFORMATION; DO NOT CONFIGURE
+
+local Information = {
+
+	Portal = 5470658045,
+	
+	Version_Name = "Vanilla",
+	
+	V = "1.0.0",
+
+	Center_Type = "Support A Creator Centers",
+	
+	Package = 0,
+
+	Add_Ons = {
+		
+		{0, 0};
+		
+	},
+
+}
+
+return Information
+
+--> Copyright 2020-2021 Trojan Technologies. © All rights reserved.
